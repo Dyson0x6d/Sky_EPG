65a062f0b559dbf7d55ddaf7c15950f9acd3c893
DICEH0MIO\dyson <ray.knubbes@aol.com>
2023-09-28 22:19:41 +0100

    add eligibility_service_test to EPG_Unit_Tests
    include 3 tests to handle valid, invalid and technical failure situations.
    
    Use QSignalSpy to detect the sendEligibility signal raised by checkAccountEligibility()
    
    use qvariant_cast to convert to enum EligibilityServiceType

--- EPG_Unit_Tests/tst_rewards_service.cpp
+++ EPG_Unit_Tests/tst_rewards_service.cpp
@@ -3,6 +3,9 @@
 // add necessary includes here
 #include "RewardsServiceObject.h"
 #include "ChannelObject.h"
+#include "EligibilityServiceStubObject.h"
+
+//========================================================================
 
 class rewards_service_test : public QObject
 {
@@ -43,11 +46,77 @@ void channel_object_test::testChannelObjectConstructor()
     QCOMPARE_NE( channel.getChannelName(), "");
 }
 
+
+//========================================================================
+
+class eligibility_service_test : public QObject
+{
+    Q_OBJECT
+public:
+    eligibility_service_test() {}
+    ~eligibility_service_test() {}
+
+private slots:
+    void initTestCase() {};
+    void cleanupTestCase() {};
+
+    void testValidAccountNumber();
+    void testInvalidAccountNumber();
+    void testTechnicalFailureAccountNumber();
+
+};
+
+void eligibility_service_test::testValidAccountNumber()
+{
+    qRegisterMetaType<EligibilityServiceType>();
+    QSignalSpy spy(&(EligibilityServiceStubObject::GetInstance()),
+                  SIGNAL( sendEligibility(EligibilityServiceType) ) );
+
+    EligibilityServiceStubObject::GetInstance().checkAccountEligibility(300);
+
+    QCOMPARE(spy.count(), 1);
+    EligibilityServiceType estReturn = qvariant_cast<EligibilityServiceType>(spy.at(0).at(0));
+
+    QVERIFY(estReturn == CUSTOMER_ELIGIBLE);
+}
+
+void eligibility_service_test::testInvalidAccountNumber()
+{
+    qRegisterMetaType<EligibilityServiceType>();
+    QSignalSpy spy(&(EligibilityServiceStubObject::GetInstance()),
+                   SIGNAL( sendEligibility(EligibilityServiceType) ) );
+
+    EligibilityServiceStubObject::GetInstance().checkAccountEligibility(301);
+
+    QCOMPARE(spy.count(), 1);
+    EligibilityServiceType estReturn = qvariant_cast<EligibilityServiceType>(spy.at(0).at(0));
+
+    QVERIFY(estReturn == CUSTOMER_INELIGIBLE);
+}
+
+void eligibility_service_test::testTechnicalFailureAccountNumber()
+{
+    qRegisterMetaType<EligibilityServiceType>();
+    QSignalSpy spy(&(EligibilityServiceStubObject::GetInstance()),
+                   SIGNAL( sendEligibility(EligibilityServiceType) ) );
+
+    EligibilityServiceStubObject::GetInstance().checkAccountEligibility(302);
+
+    QCOMPARE(spy.count(), 1);
+    EligibilityServiceType estReturn = qvariant_cast<EligibilityServiceType>(spy.at(0).at(0));
+
+    QVERIFY(estReturn == TECHNICAL_FAILURE);
+}
+
+
+//========================================================================
+
 int main(int argc, char *argv[])
 {
     int status = 0;
     //status |= QTest::qExec(new rewards_service_test, argc, argv);
     status |= QTest::qExec(new channel_object_test, argc, argv);
+    status |= QTest::qExec(new eligibility_service_test, argc, argv);
     // status |= QTest::qExec(new ..., argc, argv);
 
     return status;
